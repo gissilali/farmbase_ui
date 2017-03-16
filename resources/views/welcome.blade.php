aec49009cd7676935b7016aa8a01a744a821492c
Silali <gibsonsilali@gmail.com>
2017-03-16 23:22:24 +0300

    welcome page done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,62 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+@extends('layouts.app')
+@section('content')
+    <div id="welcome">
+        <section class="main-banner">
+        <div class="container container-custom">
+            <div class="center-box">
+                <h1 class="title">Farmbase</h1>
+                <h4 class="sub-title">i need a really cool line here...</h4>
+                <p>Farmbase is a web based service aimed at making agricultural ads easier to manage it kinda like olx for farmers</p>
+                <a href="" class="btn cta-get-started btn-lg">start creating ad</a>
+                <div style="font-size:24px;padding-top:25vh;text-align: center;">
+                    <h3>Browse</h3>
+                    <a href="#"><i class="fa fa-arrow-down" style="color:white"></i></a>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </section>
+    <div class="item-grid">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="categories-panel clearfix">
+                    <div class="container">
+                        <ul class="tabs">
+                            <li class="current"><a href="">farm produce</a></li>
+                            <li><a href="">farm implements</a></li>
+                            <li><a href="">other stuff</a></li>
+                            <li><a href="">top ten</a></li>
+                            <li><a href="">crap show</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                   <div class="panel item">
+                       <div class="panel-heading image" style="background:#d8d8d8;background-size:cover;height:250px;">
+                            <div class="no-image"><i class="fa fa-image"></i>
+                                <h4>no image available</h4>
+                            </div>
+                           <span class="category label">Farm Produce</span>
+                       </div>
+                       <div class="panel-body">
+                            <h4 class="title">Kill Bill</h4>
+                            <div class="item-info">
+                                <p class="location"><i class="fa fa-map-marker"></i>Kisumu</p>
+                                <p class="price">3000 KES</p>
+                            </div>
+
+                       </div>
+                       <div class="panel-footer">
+                           <p class="author"><i class="fa fa-user"></i>Kill Bill</p>
+                           <button class="fa fa-star favorite"></button>
+                       </div>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
